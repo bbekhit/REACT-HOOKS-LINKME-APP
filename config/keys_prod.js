54d67bc1bc67d0353adf7a2dca72ee9c30d65e80
Boutros Bekhit <bbekhit@apple.com>
2019-11-23 14:10:33 -0800

    first commit

---
+++ config/keys_prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+  mongoURI: process.env.MONGO_URI,
+  jwtSecret: process.env.SECRET_OR_KEY,
+  githubClientId: process.env.GITHUB_CLIENT_ID,
+  githubSecret: process.env.GITHUB_SECRET
+};
